530308b6a2131c7736e3251f00f4d9fdb018d8b7
Brennan Willingham <brennanw22@gmail.com>
2015-11-21 11:08:13 -0600

    Working rust excercise.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-#[test]
-fn it_works() {
+pub fn main() {
+    let vec = fill_vec(Vec::new());
+
+    println!("vec has {:?} (length is {})", vec, vec.len());
+}
+
+fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+    let mut vec = vec;
+
+    vec.push(22);
+    vec.push(44);
+    vec.push(66);
+
+    return vec.clone();
 }
